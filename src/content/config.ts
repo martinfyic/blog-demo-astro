633910e2dc64a1d0197ea0116eb76f5c3fe9f937
martinfyic <martin.f.yic@gmail.com>
2024-10-25 22:10:23 -0300

    feat: :sparkles: agrego collecion de author

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -12,11 +12,29 @@ const blogCollection = defineCollection({
         message: "Imagen debe ser menor a 1200px",
       }),
 
-      author: z.string(),
+      author: reference("author"),
+
       tags: z.array(z.string()),
+
+      isDraft: z.boolean().default(false),
+    }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
     }),
 });
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
